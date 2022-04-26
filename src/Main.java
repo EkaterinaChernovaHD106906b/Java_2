bcc37d80a041077a3311aaa62d2f1a63461bd624
Екатерина Чернова <hd106906bqwerty@gmail.com>
2022-04-26 18:10:18 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 5000;
+        int rub = 20;
+        int miles = price / rub;
+
+        System.out.println("Miles: " + miles);
+    }
+}
